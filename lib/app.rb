48013f28495d7e52f05ba0077f4726177071d876
Rzbck <chauvel.charlie@gmail.com>
2020-04-15 16:40:55 +0200

    updated

--- lib/app.rb
+++ lib/app.rb
@@ -6,10 +6,10 @@ Dotenv.load('../.env') if Dotenv.load('.env').empty?
 
 def client
   Twitter::REST::Client.new do |config|
-    config.consumer_key        = ENV[TWITTER_CONSUMER_KEY]
-    config.consumer_secret     = ENV[TWITTER_CONSUMER_SECRET]
-    config.access_token        = ENV[TWITTER_ACCESS_TOKEN]
-    config.access_token_secret = ENV[TWITTER_ACCESS_TOKEN_SECRET]
+    config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+    config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+    config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+    config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
   end
 end
 
